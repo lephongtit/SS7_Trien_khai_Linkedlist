a70d0d3978f6f3987da260a59150b33ad9eeadc3
Lê Hoàng Thiên Phong <thienphong251199@gmail.com>
2019-08-09 09:26:35 +0700

    'oke'

---
+++ src/TestMyLinkedList.java
@@ -0,0 +1,13 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList myLinkedList =new MyLinkedList(10);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+
+        myLinkedList.add(4,8);
+        myLinkedList.add(4,9);
+        myLinkedList.printList();
+    }
+}
